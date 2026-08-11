391b0f5edaa254e0ce1df7d6e46d93f8e32766a7
GPerisse <perisseguilhermeg@gmail.com>
2026-08-11 11:42:50 -0300

    feat: cria componente da paginação e adiciona na pagina de produtos e adicccciona novas imagens para os produtos

---
+++ components/paginacao/index.tsx
@@ -0,0 +1,28 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+interface PaginacaoProps {
+    paginaAtual: number;
+    totalPaginas: number;
+    onPageChange: (pagina: number) => void;
+}
+
+export default function Paginacao({ paginaAtual, totalPaginas, onPageChange }: PaginacaoProps) {
+    if (totalPaginas <= 1) return null;
+    const paginas = Array.from({ length: totalPaginas }, (_, index) => index + 1);
+
+    return (
+        <div className="flex items-center justify-center gap-2 mt-8">
+            <button onClick={() => onPageChange(paginaAtual - 1)} disabled={paginaAtual === 1} className="p-2 rounded-lg bg-[#171A1D] border border-[#3B3B40] text-[#C0C0C0] hover:bg-[#202428] disabled:opacity-50 disabled:cursor-not-allowed transition-colors flex items-center justify-center cursor-pointer">
+                <ChevronLeft size={20} />
+            </button>
+            {paginas.map((pagina) => (
+                <button key={pagina} onClick={() => onPageChange(pagina)} className={`w-10 h-10 rounded-lg flex items-center justify-center font-bold text-sm transition-colors  ${paginaAtual === pagina ? "bg-[#1473CD] text-white border border-[#1473CD]" : "bg-[#171A1D] border border-[#3B3B40] text-[#C0C0C0] hover:bg-[#202428] cursor-pointer"}`}>
+                    {pagina}
+                </button>
+            ))}
+            <button onClick={() => onPageChange(paginaAtual + 1)} disabled={paginaAtual === totalPaginas} className="p-2 rounded-lg bg-[#171A1D] border border-[#3B3B40] text-[#C0C0C0] hover:bg-[#202428] disabled:opacity-50 disabled:cursor-not-allowed transition-colors flex items-center justify-center cursor-pointer">
+                <ChevronRight size={20} />
+            </button>
+        </div>
+    );
+}
--- components/produtos-page/index.tsx
+++ components/produtos-page/index.tsx
@@ -2,17 +2,19 @@
 import { useState } from "react";
 import { Search, ChevronDown } from "lucide-react";
 import ProdutoCard from "../produto-card";
+import Paginacao from "../paginacao";
 export default function ProdutosPage() {
     const [busca, setBusca] = useState("");
     const [franquia, setFranquia] = useState("Todas");
-    const [ordenacao, setOrdenacao] = useState("Relevância");
+    const [ordenacao, setOrdenacao] = useState("Mais Vendidos");
+    const [paginaAtual, setPaginaAtual] = useState(1);
     const MOCK_PRODUTOS = [
         {
             id: 1,
             nome: "Ezio Auditore",
             franquia: "Games",
             descricao: "Traga a lenda da Irmandade dos Assassinos para a sua coleção! O Funko Pop do Ezio Auditore captura com perfeição os detalhes do traje icônico visto no jogo.",
-            preco: 229.90,
+            preco: 289.90,
             imagem: "/ezio.png",
             vendas: 342
         },
@@ -21,7 +23,7 @@ export default function ProdutosPage() {
             nome: "Batman (The Dark Knight)",
             franquia: "DC Comics",
             descricao: "Boneco Funko Pop detalhado com a armadura clássica do Cavaleiro das Trevas.",
-            preco: 179.90,
+            preco: 279.90,
             imagem: "/batman.png",
             vendas: 220
         },
@@ -30,7 +32,7 @@ export default function ProdutosPage() {
             nome: "Darth Vader",
             franquia: "Star Wars",
             descricao: "O lorde sith mais temido da galáxia agora na sua coleção.",
-            preco: 219.90,
+            preco: 319.90,
             imagem: "/darthvader.png",
             vendas: 189
         },
@@ -39,7 +41,7 @@ export default function ProdutosPage() {
             nome: "Gohan Super Saiyajin",
             franquia: "Animes",
             descricao: "Eleve seu ki ao máximo com esta edição incrível do Gohan preparado para a batalha.",
-            preco: 199.90,
+            preco: 299.90,
             imagem: "/gohan.png",
             vendas: 170
         },
@@ -48,7 +50,7 @@ export default function ProdutosPage() {
             nome: "Hermione",
             franquia: "Harry Potter",
             descricao: "Defenda o mundo bruxo com essa edição incrivel da Hermione Granger",
-            preco: 159.90,
+            preco: 189.90,
             imagem: "/hermione.png",
             vendas: 80
         },
@@ -57,7 +59,7 @@ export default function ProdutosPage() {
             nome: "Homem Aranha",
             franquia: "Marvel",
             descricao: "Batalhe por Nova York com nosso herói da vizinhança",
-            preco: 169.90,
+            preco: 269.90,
             imagem: "/homemaranha.png",
             vendas: 95
         },
@@ -65,29 +67,75 @@ export default function ProdutosPage() {
             id: 7,
             nome: "Max Verstappen",
             franquia: "Esportes",
-            descricao: "Vança corridas com nosso campeão mundial Max Verstappen",
-            preco: 209.90,
+            descricao: "Vença corridas com nosso campeão mundial Max Verstappen",
+            preco: 259.90,
             imagem: "/max.png",
             vendas: 190
         },
         {
             id: 8,
-            nome: "Gohan Super Saiyajin",
-            franquia: "Animes",
-            descricao: "Eleve seu ki ao máximo com esta edição incrível do Gohan preparado para a batalha.",
-            preco: 199.90,
-            imagem: "/gohan.png",
-            vendas: 170
+            nome: "Master Chief",
+            franquia: "Games",
+            descricao: "Ajude Cortana nas batalhas no espaço com John 117 - Master Chief",
+            preco: 229.90,
+            imagem: "/chief.png",
+            vendas: 187
         },
         {
             id: 9,
-            nome: "Gohan Super Saiyajin",
-            franquia: "Animes",
-            descricao: "Eleve seu ki ao máximo com esta edição incrível do Gohan preparado para a batalha.",
-            preco: 199.90,
-            imagem: "/gohan.png",
+            nome: "Capitão America",
+            franquia: "Marvel",
+            descricao: "Lidere batalhas com o nosso grande lider dos vingadores capitão america",
+            preco: 209.90,
+            imagem: "/capitao.png",
             vendas: 170
         },
+        {
+            id: 10,
+            nome: "Lebron James",
+            franquia: "Esportes",
+            descricao: "Recrute LeBron James, do Los Angeles Lakers da NBA, para a sua coleção de basquete com este Boneco Funko POP",
+            preco: 209.90,
+            imagem: "/lebron.png",
+            vendas: 100
+        },
+        {
+            id: 11,
+            nome: "Harry Potter",
+            franquia: "Harry Potter",
+            descricao: "Derrote aquele que nao deve ser nomeado para salvar o mundo bruxo com o exclusivo Harry Potter",
+            preco: 339.90,
+            imagem: "/harry.png",
+            vendas: 79
+        },
+        {
+            id: 12,
+            nome: "Luke Skywalker",
+            franquia: "Star Wars",
+            descricao: "Adicione um ícone da galáxia à sua coleção com este herói lendário da Aliança Rebelde",
+            preco: 309.90,
+            imagem: "/luke.png",
+            vendas: 138
+        },
+        {
+            id: 13,
+            nome: "Naruto",
+            franquia: "Animes",
+            descricao: "Defenda a vila da folha com o maior shinobe da atualidade, Naruto Uzumaki",
+            preco: 259.90,
+            imagem: "/naruto.png",
+            vendas: 229
+        },
+        {
+            id: 14,
+            nome: "Superman",
+            franquia: "DC Comics",
+            descricao: "O grande Superman, herói da liga da justiça",
+            preco: 249.90,
+            imagem: "/superman.png",
+            vendas: 209
+        },
+
     ];
         const produtosFiltrados = MOCK_PRODUTOS.filter((produto) => {
         const bateComBusca = produto.nome.toLowerCase().includes(busca.toLowerCase()) || produto.franquia.toLowerCase().includes(busca.toLowerCase());
@@ -96,13 +144,18 @@ export default function ProdutosPage() {
     });
 
     produtosFiltrados.sort((a, b) => {
-        if (ordenacao === "Relevância") return b.vendas - a.vendas;
+        if (ordenacao === "Mais Vendidos") return b.vendas - a.vendas;
         if (ordenacao === "Menor Preço") return a.preco - b.preco;
         if (ordenacao === "Maior Preço") return b.preco - a.preco;
         if (ordenacao === "A-Z") return a.nome.localeCompare(b.nome);
         return 0;
-    });
-    const produtosPaginados = produtosFiltrados.slice(0, 8);
+    })
+
+    const itensPorPagina = 8;
+    const totalPaginas = Math.ceil(produtosFiltrados.length / itensPorPagina);
+    const indexInicial = (paginaAtual - 1) * itensPorPagina;
+    const indexFinal = indexInicial + itensPorPagina;
+    const produtosPaginados = produtosFiltrados.slice(indexInicial, indexFinal);
     return (
         <div className="w-full min-h-screen bg-[#0D0F11] text-[#F5F5F5] flex flex-col items-center py-12 px-6">
            <div className="w-full max-w-7xl flex flex-col gap-8">
@@ -112,11 +165,11 @@ export default function ProdutosPage() {
                     </h1>
                     <div className="flex flex-col md:flex-row gap-4 w-full md:w-auto">
                         <div className="relative w-full md:w-64">
-                            <input type="text" placeholder="Pesquisar Funko..." value={busca} onChange={(e) => setBusca(e.target.value)} className="w-full bg-[#171A1D] border border-[#3B3B40] rounded-lg py-3 pl-4 pr-10 text-sm focus:outline-none focus:border-[#1473CD] transition-colors"/>
+                            <input type="text" placeholder="Pesquisar Funko..." value={busca} onChange={(e) => {setBusca(e.target.value); setPaginaAtual(1);}} className="w-full bg-[#171A1D] border border-[#3B3B40] rounded-lg py-3 pl-4 pr-10 text-sm focus:outline-none focus:border-[#1473CD] transition-colors"/>
                             <Search size={18} className="absolute right-3 top-1/2 -translate-y-1/2 text-[#C0C0C0]" />
                         </div>
                         <div className="relative w-full md:w-auto">
-                            <select value={franquia} onChange={(e) => setFranquia(e.target.value)} className="w-full bg-[#171A1D] border border-[#3B3B40] rounded-lg py-3 pl-4 pr-10 text-sm focus:outline-none focus:border-[#1473CD] cursor-pointer appearance-none hover:bg-[#202428]">
+                            <select value={franquia} onChange={(e) => {setFranquia(e.target.value); setPaginaAtual(1); }} className="w-full bg-[#171A1D] border border-[#3B3B40] rounded-lg py-3 pl-4 pr-10 text-sm focus:outline-none focus:border-[#1473CD] cursor-pointer appearance-none hover:bg-[#202428]">
                                 <option value="Todas">Todas as Categorias</option>
                                 <option value="Star Wars">Star Wars</option>
                                 <option value="Harry Potter">Harry Potter</option>
@@ -129,8 +182,8 @@ export default function ProdutosPage() {
                             <ChevronDown size={18} className="absolute right-3 top-1/2 -translate-y-1/2 text-[#C0C0C0] pointer-events-none" />
                         </div>
                         <div className="relative w-full md:w-auto">
-                            <select value={ordenacao} onChange={(e) => setOrdenacao(e.target.value)} className="w-full bg-[#171A1D] border border-[#3B3B40] rounded-lg py-3 pl-4 pr-10 text-sm focus:outline-none focus:border-[#1473CD] cursor-pointer appearance-none hover:bg-[#202428]">
-                                <option value="Relevância">Relevância</option>
+                            <select value={ordenacao} onChange={(e) => {setOrdenacao(e.target.value); setPaginaAtual(1);}} className="w-full bg-[#171A1D] border border-[#3B3B40] rounded-lg py-3 pl-4 pr-10 text-sm focus:outline-none focus:border-[#1473CD] cursor-pointer appearance-none hover:bg-[#202428]">
+                                <option value="Mais Vendidos">Mais Vendidos</option>
                                 <option value="Menor Preço">Menor Preço</option>
                                 <option value="Maior Preço">Maior Preço</option>
                                 <option value="A-Z">Nome (A-Z)</option>
@@ -153,6 +206,11 @@ export default function ProdutosPage() {
                         />
                     ))}
                 </div>
+                <Paginacao 
+                    paginaAtual={paginaAtual} 
+                    totalPaginas={totalPaginas} 
+                    onPageChange={(novaPagina) => setPaginaAtual(novaPagina)} 
+                />
             </div>
         </div>
     );
